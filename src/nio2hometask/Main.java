6b67620e8f0242cfa2f06db655485690e2fcf704
Svetlana Litovchenko <litovchenkosv1990@gmail.com>
2018-02-23 13:47:06 +0200

    Edited serialization task

--- src/nio2hometask/Main.java
+++ src/nio2hometask/Main.java
@@ -8,6 +8,7 @@ import java.util.ListIterator;
 
 import static java.nio.file.StandardCopyOption.*;
 
+
 public class Main {
     /*
     Задание:
@@ -16,32 +17,29 @@ public class Main {
     3. Применить методы копирования и перемещения файлов
     4. Применить сервис отслеживаний
 
-//Устранить появление исключений!!!
-
 
      */
 
 
     public static void main(String[] args) throws IOException {
+        Path taskDir = Paths.get(".\\NioTask");
+        if (!Files.exists(taskDir)) {
+            Files.createDirectory(taskDir);
+        } else {
+            System.out.println("Папка " + taskDir.toRealPath() + " уже существует");
+        }
+        Thread th = new Thread(new MyWatchService(".\\NioTask"));
+        th.start();
 
 
         //1. создаю файл
-        Path novelDoc = Paths.get(".\\Stephen King.Novels.Main.txt");
-        if (!Files.exists(novelDoc)){
-            Files.createFile(novelDoc);
-        }else{
-            System.out.println("Файл по указанному пути уже существует\n Перезаписываю");
 
+        Path novelDoc = Paths.get(".\\NioTask\\Stephen King.Novels.Main.txt");
+        if (!Files.exists(novelDoc)) {
+            Files.createFile(novelDoc);
+        } else {
+            System.out.println("Файл по указанному пути уже существует\n Перезаписываю существующий файл");
         }
-        //Files.exists()
-        /*try {
-            Files.createFile(novelDoc);
-        } catch (FileAlreadyExistsException e) {
-            //   e.printStackTrace();
-            System.out.println("Указанный файл уже существует");
-        }*/
-
-
 
 
         //2. Записать данные в файл и считать данные из файла. Вывести считанное в консоль
@@ -56,63 +54,89 @@ public class Main {
         novels.add("Ловец снов");
         novels.add("Под куполом");
 
-        // Charset charset=Charset.forName("UTF-8");
         //записать данные в файл
-        BufferedWriter writer = null;
-        try {
-            // BufferedWriter writer= Files.newBufferedWriter(novelDoc,charset);
-           // writer = Files.newBufferedWriter(novelDoc, StandardCharsets.UTF_8, StandardOpenOption.WRITE, StandardOpenOption.APPEND);
-            writer = Files.newBufferedWriter(novelDoc, StandardCharsets.UTF_8, StandardOpenOption.WRITE);
+        try (BufferedWriter writer = Files.newBufferedWriter(novelDoc, StandardCharsets.UTF_8, StandardOpenOption.WRITE)) {
             ListIterator<String> li = novels.listIterator();
             while (li.hasNext()) {
-                writer.write(li.next() + "\n");
+                writer.write(li.next());
+                writer.newLine();
             }
-
-
         } catch (IOException io) {
             io.printStackTrace();
-        } finally {
-            writer.flush();
-            writer.close();
         }
 
 
-       // LinkedList<String> novelsFromFile=new LinkedList<>();
+        // LinkedList<String> novelsFromFile=new LinkedList<>();
         //считать данные из файла:
-        try (BufferedReader reader=Files.newBufferedReader(novelDoc,StandardCharsets.UTF_8)){
+        try (BufferedReader reader = Files.newBufferedReader(novelDoc, StandardCharsets.UTF_8)) {
             String line;
-            while ((line=reader.readLine())!=null){
-         //       novelsFromFile.add(line);
+            while ((line = reader.readLine()) != null) {
+                //       novelsFromFile.add(line);
                 //вывести считанные данные в консоль
                 System.out.println(line);
-
             }
-        }catch (IOException io){
+        } catch (IOException io) {
             io.printStackTrace();
         }
 
         //3 Применить методы копирования и перемещения файлов
         //создаю директорию. копирую в нее файл
+        Path backupDir = Paths.get(".\\NioTask\\KingBackupFiles");
+        if (!Files.exists(backupDir)) {
+            Files.createDirectory(backupDir);
+        } else {
+            System.out.println("Папка " + backupDir.toRealPath() + " уже существует");
+        }
 
 
-        Path backupDir=Files.createDirectory(Paths.get(".\\KingBackupFiles"));
-        Path backupFile=Paths.get(backupDir.toString(),"\\King Novels Backup.txt");
-        Path backupFile2=Paths.get(backupDir.toString(),"\\King Novels Backup2.txt");
-        Path backupFile3=Paths.get(backupDir.toString(),"\\King Novels Backup3.txt");
+        Path backupFile = Paths.get(backupDir.toString(), "\\Novels Backup.txt");
+        Path backupFile2 = Paths.get(".\\NioTask\\KingBackupFiles\\Novels Backup2.txt");
+        Path backupFile3 = Paths.get(".\\NioTask\\KingBackupFiles", "\\Novels Backup3.txt");
+
+        //МЕТОД RESOLVE почему-то не работает. Никаких исключений не возникает, но файлы не рзздаются
+
+
+
+
+        /*//Path backupFile2 = Paths.get(".\\KingBackupFiles","\\Novels Backup2.txt");
+        // Path backupFile2=Paths.get("\\Novels Backup.txt").resolve(backupDir);*/
+        /*Path backupFile = backupDir.resolve(Paths.get("\\Novels Backup.txt"));
+        Path backupFile2 = backupDir.resolve("\\Novels Backup2.txt");
+        Path backupFile3 = backupDir.resolve("\\Novels Backup3.txt");*/
+
+
+
         //скопировать файл
-        Files.copy(novelDoc,backupFile);
-        Files.copy(novelDoc,backupFile2);
-        Files.copy(novelDoc,backupFile3);
+        /*if (!Files.exists(backupFile)) {
+            Files.copy(novelDoc, backupFile);// Заменила этот и подобные if на REPLACE EXISTING
+            System.out.println("Копирую backupFile1");
+        }*/
+        /*if (!Files.exists(backupFile2)) {
+            Files.copy(novelDoc, backupFile2);
+        }*/
+        /*if (!Files.exists(backupFile3)) {
+            Files.copy(novelDoc, backupFile3);
+        }*/
+
+        Files.copy(novelDoc, backupFile, REPLACE_EXISTING);
+        Files.copy(novelDoc, backupFile2, REPLACE_EXISTING);
+        Files.copy(novelDoc, backupFile3, REPLACE_EXISTING);
 
 
         //переместить файл
-        Path mainDir=Files.createDirectory(Paths.get(".\\KingMainFiles"));
-        Path nextMainFile=Files.createFile(Paths.get(".\\KingMainFiles\\King Novels Main.txt"));
-        Files.move(novelDoc,nextMainFile,ATOMIC_MOVE);
-
+        Path mainDir = Paths.get(".\\NioTask\\KingMainFiles");
+        if (!Files.exists(mainDir)) {
+            Files.createDirectory(mainDir);
+        }
+        Path nextMainFile = Paths.get(".\\NioTask\\KingMainFiles\\Novels Main.txt");
+        if (!Files.exists(nextMainFile)) {
+            Files.createFile(nextMainFile);
+        }
+        Files.move(novelDoc, nextMainFile, ATOMIC_MOVE);
 
         //1.удаляю файл
-       Files.delete(backupFile2);
+        Files.delete(backupFile2);
+
 
     }
 
---
+++ src/nio2hometask/MyWatchService.java
@@ -0,0 +1,95 @@
+package nio2hometask;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+public class MyWatchService implements Runnable {
+
+    //4. Применить сервис отслеживаний
+    /*public static WatchService getWatchService() throws IOException {*/
+    private Path watchAfter = Paths.get(".");
+    //private Object[] events={ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE, OVERFLOW};
+
+    public MyWatchService() {
+
+    }
+
+
+
+
+    public MyWatchService(String watchAfterPath) {
+        //в закомментированном варианте мне не нравится то, что четко не понятно, то ли папка/файл не существует,
+        // то ли вообще что-то левое передано в параметр конструктора
+
+       /* if (watchAfterPath == null || watchAfterPath.length() == 0||!Files.exists(Paths.get(watchAfterPath))){
+            throw new IllegalArgumentException("Illegal value: Passed path doesn't exist");
+        }*/
+        if (watchAfterPath == null || watchAfterPath.length() == 0){
+            throw new IllegalArgumentException("Illegal value: passed watchAfterPath is null or empty");
+        }
+        if (!Files.exists(Paths.get(watchAfterPath))) {
+            throw new IllegalArgumentException("Passed path doesn't exist");
+        }
+
+            watchAfter = Paths.get(watchAfterPath);
+
+        }
+
+
+
+    public void run() {
+        {
+            WatchService watchService = null;
+            //Path dirToWatch = FileSystems.getDefault().getPath(".");
+            try {
+                watchService = FileSystems.getDefault().newWatchService();
+                watchAfter.register(watchService, ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE, OVERFLOW);
+
+                // WatchKey watchKey = dirToWatch.register(watchService, ENTRY_MODIFY,ENTRY_CREATE,ENTRY_DELETE,OVERFLOW);
+                //while (!shutdown){// Что за shutdown  в примере?? зачем?
+
+                //}
+
+            } catch (IOException io) {
+                io.printStackTrace();
+                // throw io;
+            }
+
+            for (; ; ) {     //Warning:(60, 13) 'for' statement cannot complete without throwing an exception
+                WatchKey key = null;
+                try {
+                    key = watchService.take(); //Warning:(63, 40) Method invocation 'take' may produce 'java.lang.NullPointerException'
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+
+                for (WatchEvent event : key.pollEvents()) { //Warning:(68, 45) Method invocation 'pollEvents' may produce 'java.lang.NullPointerException'
+                    switch (event.kind().name()) {
+                        case "ENTRY_CREATE":
+                            System.out.println("File " + event.context() + " was created");
+                            break;
+                        case "ENTRY_MODIFY":
+                            System.out.println("File " + event.context() + " was modified");
+                            break;
+                        case "ENTRY_DELETE":
+                            System.out.println("File " + event.context() + " was deleted");
+                            break;
+                        case "ENTRY_OVERFLOW":
+                            System.out.println("Some events were lost");
+                            break;//этот по-моему не обязателен
+                    }
+                }
+                key.reset();
+
+            }
+        }
+
+
+    }
+
+}
+
+
+
